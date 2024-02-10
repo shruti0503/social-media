2b58b121ec04e369df09001d2245c79d1c582242
shruti0503 <shrutivishwakarma0509@gmail.com>
2024-02-11 02:29:36 +0530

    authen and authorization

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -22,6 +22,7 @@ export const register=async(req, res)=>{
         const salt=await bcrypt.genSalt()
         const passwordHash= await bcrypt.hash(password, salt)
         // encrypt pass -> save 
+        // user logs in -> give the user json web troken
         const newUser = new User({
             firstName,
             LastName,
@@ -31,13 +32,44 @@ export const register=async(req, res)=>{
             friends,
             location,
             occupation,
+            viewedProfile: Math.floor(Math.random()*10000),
+            impressions: Math.floor(Math.random()* 10000)
 
-        })
+        });
+        const savedUser =await newUser.save();
+        // send the user : 201 -> means something has been created
+        res.status(201).json(savedUser)
 
     }
     catch(err){
 
+        res.status(500).json({error:err.message})
+
     }
 
     
+}
+
+// login
+
+export const login=async(req,res)=>{
+
+    try{
+        const {email, password}=req.body
+
+        const user=await User.findOne({email:email});
+        if(!user) return res.status(400).json({msg:"user not found"})
+        const isMatch=await bcrypt.compare(password, user.password);
+       if(!isMatch) return res.status(400).json({msg:"invalid creditial"})
+
+       const token = jwt.sign({id: user._id}, process.env.JWT_SECRET)
+       delete user.password;
+
+       res.status(200).json({token,user});
+    }
+    catch(err){
+
+        res.status(500).json({error:err.message})
+
+    }
 }
