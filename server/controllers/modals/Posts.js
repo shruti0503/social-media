d2f97002cb70d1d9bf57a2f601112a5f8922a355
shrutiVishwakarma05 <shrutivishwakarma0509@gmail.com>
2024-03-13 10:27:46 +0530

    posts

--- server/controllers/modals/Posts.js
+++ server/controllers/modals/Posts.js
@@ -1,11 +1,43 @@
 // for likes : refernce of who liked it 
+import { Description } from "@mui/icons-material"
 import mongoose from "mongoose"
 const postSchema=mongoose.Schema(
     {
         userId:{
             type:String,
+            required:true,
             
-        }
+        },
+        firstName:{
+            type:String,
+            required:true,
+        },
+        lastName:{
+            type:String,
+            required:true,
+        },
+        location: String,
+        Description: String,
+        picturePath:String,
+        userPicturePath:String,
+        likes:{
+            type: Map,
+            of:Boolean,
 
-    }
-)
+        },
+        comments:{
+            type:Array,
+            default:[]
+        }
+        
+
+
+
+
+
+    },
+   { timestamps: true}
+) 
+
+const Post = mongoose.model("Post", postSchema)
+export default Post
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,1 +1,34 @@
-import Post from, 
+import Post from "./modals/posts";
+import User from "./modals/user";
+
+// create ppost 
+export const createPost = async(req,res)=>{
+    try{
+        const {userId, description, picturePath}=req.body;
+        const user=await User.findById(userId);
+        const newPost= new Post({
+            userId,
+            firstName:user.firstName,
+            lastName: user.lastName,
+            location:user.location,
+            description,
+            userPicturePath:user.picturePath,
+            picturePath,
+            likes:{},
+            comments:[]
+        })
+        await newPost.save()
+
+        const post =await Post.find();
+        res.status(201).json(post)
+
+    }
+    catch(err){
+        res.status(409).json({message:err.message})
+    }
+}
+
+// get all the posts of user
+export const getFeedPosts=async(req, res)=>{
+
+}
