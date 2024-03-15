56a5046eb77dd89b038d4dd6cf728e24a3a29de5
shrutiVishwakarma05 <shrutivishwakarma0509@gmail.com>
2024-03-16 01:24:34 +0530

    Update posts.js

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -28,7 +28,59 @@ export const createPost = async(req,res)=>{
     }
 }
 
-// get all the posts of user
+// get all the posts 
 export const getFeedPosts=async(req, res)=>{
+    try{
+        const post=await Post.find();
+        res.status(200).json(post)
+
+    }
+    catch(err){
+        res.status(404).json({message:err.message})
+    }
 
 }
+
+export const getUserFeedPosts=async(req, res)=>{
+    try{
+        const {userId}=req.params;
+        const posts=await Post.find({userId});
+        res.status(200).json(posts)
+
+    }
+    catch(err){
+        res.status(404).json({message:err.message})
+    }
+
+}
+
+// update
+export const likePost=async(req, res)=>{
+    try{
+        const {id}=req.params;
+        const {userId}=req.body;
+        const post=await Post.findById(userId)
+        const isLiked=post.likes.get(userId) // if is likes
+
+        if(isLiked){
+            post.likes.delete(userId)
+        }
+        else{
+            post.likes.set(userId, true)
+        }
+
+        const updatedPost=await Post.findByIdAndUpdate(
+            id,
+            {likes:post.likes},
+            {new:true}
+        )
+
+        res.send(200).json(updatedPost)
+
+    }
+    catch(err){
+        res.status(404).json({message:err.message})
+    }
+
+}
+
