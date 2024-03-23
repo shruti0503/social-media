855f2635d6ebdf9f7e0cc33dc517d3e20016f923
shrutiVishwakarma05 <shrutivishwakarma0509@gmail.com>
2024-03-23 14:18:47 +0530

    sum

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 
-
-
+import {BrowserRouter, Navigate, Routes, Route} from "react-router-dom"
+import HomePage from "./scenes/HomePagee";
+import LoginPage from "./scenes/LoginPage";
+import ProfilePage from "./scenes/ProfilePage";
+import {useMemo} from "react"
+import {useSelector} from "react-redux"
+import {CssBaseline, ThemeProvider} from "@mui/material"
+import {createTheme} from "@mui/material/styles"
+import { themeSettings } from "./theme";
 function App() {
+  const mode=useSelector((state)=> state.mode)
   return (
     <div className="App">
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage/>} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/profile/:userId" element={<ProfilePage />} />
+      </Routes>
+      </BrowserRouter>
   
     </div>
   );
---
+++ client/src/scenes/NavBar/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>LoginPage</div>
+  )
+}
+
+export default NavBar
